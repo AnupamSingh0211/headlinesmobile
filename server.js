5fab0baebf11f173b3ce48ea977e6137c879e035
anupamsingh <anupamsingh@urbanclap.com>
2018-05-26 23:01:39 +0530

    /.well-known/assestlinks.json url is working now

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var jsonFile = require(path.join(__dirname + '/assetlinks.json'));
+console.log(`thats its${jsonFile}`);
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static('public'));
 
 app.get('/assetlinks.json', function(req, res) {
-	console.log(path.join(__dirname + '/assetlinks.json'));
-  res.render(path.join(__dirname + '/assetlinks.json'));
+    console.log(path.join(__dirname + '/assetlinks.json'));
+  res.send(jsonFile);
 });
 
 app.listen(app.get('port'), function() {
